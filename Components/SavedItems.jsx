ff38b483c5d5c8729c6d8f680698501fc5a93905
Robert Horngren <robert@horngren.net>
2021-08-30 19:28:18 +0200

    improved history view of past calculations

--- Components/SavedItems.jsx
+++ Components/SavedItems.jsx
@@ -1,21 +1,21 @@
 // component to display and manage previously saved items
 
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { ScrollView, Text, View } from "react-native";
 
 //demonstrating the use of props instead of context
 const SavedItems = (props) => {
+  const [history, setHistory] = useState([]);
 
   useEffect(() => {
-    console.log('props.pastResult: ', props.pastResults);
+    setHistory(props.pastResults.reverse());
   }, [props.pastResults])
-
-  //TODO: invert order of props.pastResults for most recent result first
 
   return (
     <ScrollView style={{ height: 50, width: "100%", backgroundColor: "gray" }}>
-      {props.pastResults.length > 0 ?
+      {history.length > 0 ?
         props.pastResults.map((item, index) =>
           <View key={index}>
             <Text>{item}</Text>
