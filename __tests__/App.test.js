ea7cb536de0962ebd8c6d5cd3c10d253d8e06326
Robert Horngren <robert@horngren.net>
2021-09-02 11:15:54 +0200

    updated app.test.js to test all core components

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,10 +1,16 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import DisplayComponent from '../Components/DisplayComponent';
+import ButtonComponent from '../Components/ButtonComponent';
+import HistoryComponent from '../Components/HistoryComponent';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('core components renders correctly', () => {
   renderer.create(<App />);
+  renderer.create(<DisplayComponent />);
+  renderer.create(<ButtonComponent />);
+  renderer.create(<HistoryComponent />);
 });
